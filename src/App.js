d434b57d8ef2a06f4c173cf04b40be532b61c794
AurobindoGupta <aurogpt10@gmail.com>
2021-05-11 21:39:19 +0530

    fetch movie details from api and diplay

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
+import Movie from './component/Movie';
+
+const Featured_API =
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1c40073fce452c87ff4ca4bee5877ecf&page=1";
+
+const Search_API =
+    "https://api.themoviedb.org/3/search/movie?&api_key=1c40073fce452c87ff4ca4bee5877ecf&query=";
+
+
 function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(  () => {
+    fetch(Featured_API).then(res => res.json()).then(data => {
+      console.log(data);
+      setMovies(data.results);
+    });
+    
+    
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    { movies.length > 0 && movies.map((movie) =>(
+      <Movie key={movie.id} { ...movie}/>
+    ))}
+      
     </div>
   );
 }
